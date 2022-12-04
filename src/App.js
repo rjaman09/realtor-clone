bf920ce5c5a187c32cec452f45ee3037d6b473cf
admin <admin@MacBook-Air.local>
2022-12-04 23:13:38 +0530

    add create-listing page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
 import Offers from "./pages/Offers";
+import CreateListing from "./pages/CreateListing";
 
 // Components -->
 import Header from "./components/Header";
@@ -28,6 +29,7 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/offers" element={<Offers />} />
+          <Route path="/create-listing" element={<CreateListing />} />
         </Routes>
       </Router>
 
